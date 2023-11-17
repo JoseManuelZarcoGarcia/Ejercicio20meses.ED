57efe0c578e6b703badfc11232a8c2b708e2c9d5
DESKTOP-A37MTN2\csi22-jzarcia <jose.manuel.zarco.garcia@gmail.com>
2023-11-17 08:49:50 +0100

    no message

---
+++ src/Inicio.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+		
+		Scanner ct = new Scanner(System.in);
+		long aux = 10, mes;
+		String texto1 = "En cuantos meses se va a realizar el pago? ";
+		
+		mes = pedriNum(ct, texto1);
+		
+		for(int i = 1; i<(mes+1); i++) {
+			
+			aux = aux * 2;
+			
+			
+			
+			System.out.println("El mes " + i + " pagara: " + aux);
+		}
+		
+
+	}
+	
+	
+	static long pedriNum(Scanner ct, String texto) {
+		
+		long respuesta = 0;
+		
+		System.out.println(texto);
+		respuesta=ct.nextInt();
+		return respuesta;
+	}
+
+}
